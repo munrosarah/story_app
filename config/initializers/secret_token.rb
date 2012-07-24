0b7b87461142dd8f65a7aa9bb64700d3b7206a3b
Sarah Munro <munro.sarah@gmail.com>
2012-07-23 19:15:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryApp::Application.config.secret_token = 'd119848db5c301adc45530ec9a9c67b52a7e2e6567603f759e35ff004b4d54814d2ed67b34095c4cef7ed39b51ce815293cd1629b2d479f27ba6bd01e6674049'
